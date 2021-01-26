55f638c5c5184bd552ffa8d434b8d722b7331a31
oshcherbakov <crusader34@gmail.com>
2021-01-26 11:36:51 +0200

    some chages with dynamic block code

--- SimpleWebServer/WebServer.tf
+++ SimpleWebServer/WebServer.tf
@@ -28,6 +28,9 @@ resource "aws_instance" "my_webserver" {
     Name  = "Web Server Build by Terraform"
     Owner = "Oleksii Shcherbakov"
   }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 
@@ -35,11 +38,14 @@ resource "aws_security_group" "my_webserver" {
   name        = "WebServer Security Group"
   description = "My First SecurityGroup"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = ["80", "443"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
   egress {
--- SimpleWebServer/user_data.sh
+++ SimpleWebServer/user_data.sh
@@ -4,5 +4,6 @@ yum -y install httpd
 echo "<h2>WebServer for Sigma-Software</h2><br>Build by Terraform using External Script!"  >  /var/www/html/index.html
 echo "<br><font color="blue">Hello Everyone!!!" >> /var/www/html/index.html
 echo "<br><font color="green">made by Oleksii Shcherbakov" >> /var/www/html/index.html
+echo "<br><font color="black">version 1.0" >> /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
